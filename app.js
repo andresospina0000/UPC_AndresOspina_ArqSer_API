9bf7c530597c7d8d9942f4aaa995a0d20f7ef81b
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-05-13 21:14:22 -0500

    Agregando puntos 1,2 y 3 de la actividad 1.

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
+const employees = require('./employees');
+const express = require('express');
+const app = express();
 
+app.use(express.json());
 
+app.get('/api/employees', (req, res) => {
 
-function start(){
-    ``
-}
+    const page = req.query?.page;
+
+    if (page == 1) {
+        var result = employees.slice(0, 2);
+        res.send(result);
+        return;
+    }
+
+    if (page == 2) {
+        var result = employees.slice(2, 5);
+        res.send(result);
+        return;
+    }
+
+    res.send(employees);
+    return;
+});
+
+app.listen(8000,
+    () => console.log('Server running on port 8000')
+);
