874deb9fa1911555a73f28011390dd179d7bb147
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-05-16 17:39:31 -0500

    Agregando el comportamiento de todos los puntos con query params (puntos 2,2,3,5 y 7 ) de la actividad 1.

--- app.js
+++ app.js
@@ -6,22 +6,68 @@ app.use(express.json());
 
 app.get('/api/employees', (req, res) => {
 
-    const page = req.query?.page;
+    const queryParams = req.query;
 
-    if (page == 1) {
+    if (!queryParams) {
+        res.send(employees);
+        return;
+    }
+
+    if (queryParams.page == 1) {
         var result = employees.slice(0, 2);
+        res.send(result);
+        return;
+    } else if (queryParams.page == 2) {
+        var result = employees.slice(2, 4);
+        res.send(result);
+        return;
+    } else if (queryParams.page > 2) {
+        console.log('page > 2');
+        const from = 2 * (queryParams.page - 1);
+        const to = (2 * (queryParams.page - 1)) + 1;
+        var result = employees.slice(from, to);
         res.send(result);
         return;
     }
 
-    if (page == 2) {
-        var result = employees.slice(2, 5);
-        res.send(result);
+    if (queryParams.user == 'true') {
+        const employeesCopy = Array.from(employees)
+        const onlyUsers = employeesCopy.filter(employee => {
+            return employee.privileges == 'user'
+        });
+
+        res.send(onlyUsers);
+        return;
+    }
+
+    if (queryParams.badges == 'black') {
+        const employeesCopy = Array.from(employees)
+        const blackBadges = employeesCopy.filter(employee => {
+            const employeeBadges = Array.from(employee.badges);
+            return employeeBadges.includes(queryParams.badges)
+        });
+
+        res.send(blackBadges);
         return;
     }
 
     res.send(employees);
     return;
+});
+
+app.get('/api/employees/oldest', (req, res) => {
+    const oldest = employees.reduce((prev, current) => {
+        return (prev.age > current.age) ? prev : current
+    });
+
+    res.send(oldest);
+});
+
+app.get('/api/employees:?user', (req, res) => {
+    console.log('user?');
+    const employeesCopy = Array.from(employees)
+    console.log(employeesCopy);
+    res.send(employeesCopy);
 });
 
 app.listen(8000,
