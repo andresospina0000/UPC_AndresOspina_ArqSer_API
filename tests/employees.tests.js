9e122cccdb569f353b82c9b1a305301754a306a5
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-05-20 16:35:10 -0500

    Agregando un par de test cases con supertest.

---
+++ tests/employees.tests.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../app');
+const assert = require('assert');
+const http = require('http');
+const testServer = http.createServer(app);
+
+request(testServer)
+    .get('/api/employees')
+    .expect(200)
+    .end(function (err, res) {
+        if (err) throw err;
+        expect(res.body.length).toBe(3);
+    });
+
+request(testServer)
+    .get('/api/employees?user=true')
+    .expect(200)
+    .end(function (err, res) {
+        if (err) throw err;        
+        assert.equal(res.body.length, 4);
+        assert.equal(res.body[0].name, 'Sue');
+        assert.equal(res.body[1].name, 'Willy');
+        assert.equal(res.body[2].name, 'Steve');
+        assert.equal(res.body[3].name, 'Martin');
+    });
+
+
