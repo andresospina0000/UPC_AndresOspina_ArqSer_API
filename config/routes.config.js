217ba9792d64d99c6dd4503c0ba28ca7096ea4ec
Andres Felipe Ospina Lenis <andresospina0000@gmail.com>
2023-05-22 23:51:45 -0500

    Implementacion de la actividad 4: middleware para usuarios no activos => no podran loguearse ni tampoco podran utilizar los endpoinst /posts.

--- config/routes.config.js
+++ config/routes.config.js
@@ -19,7 +19,7 @@ router.delete('/posts/:id', authMiddleware.isAuthenticated, postsController.dele
 
 // CRUD users
 router.post('/users', usersController.create);
-router.post('/login', usersController.login);
+router.post('/login', authMiddleware.isActive, usersController.login);
 router.get('/users/:id/validate', usersController.validate);
 
 
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const User = require('../models/user.model');
 const jwt = require('jsonwebtoken');
 
 module.exports.isAuthenticated = (req, res, next) => {
-    const token = req.header('Authorization').split(' ')[1];
+    const token = req.header('Authorization')?.split(' ')[1];
     if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if (err) {
@@ -31,4 +31,23 @@ module.exports.isAuthenticated = (req, res, next) => {
     else {
         res.status(401).json({ message: 'Unauthorized: Token not found' });
     }
+}
+
+module.exports.isActive = (req, res, next) => {
+
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: 'Unauthorized: User not found!' })
+            }
+            else {
+                if (!user.active) {
+                    res.status(401).json({ message: 'Unauthorized: User not active!' })
+                }
+                else {
+                    next();
+                }
+            }
+        })
+        .catch(next)
 }
